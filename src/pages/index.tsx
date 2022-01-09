7877dfb5a409a0e1ecc25201e02c634efa1985a4
= <=>
2022-01-08 21:56:17 -0300

    add global style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
+import Head from "next/head";
+
 export default function Home() {
-  return <h1>Hello World Next</h1>;
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1>Hello World Next</h1>
+    </>
+  );
 }
