da65b677265c8bcb18c7339f9dbb7c8fdf9830fa
= <=>
2022-01-14 17:20:39 -0300

    next auth github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,30 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const userLoggedIn = true;
+  const { data: session } = useSession();
 
-  return userLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  console.log(session);
+
+  return session ? (
+    <button
+      onClick={() => signOut()}
+      type="button"
+      className={styles.signInButton}
+    >
       <FaGithub color="#04d321" />
-      Gian Lucas
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      onClick={() => signIn("github")}
+      type="button"
+      className={styles.signInButton}
+    >
       <FaGithub color="#eba417" />
       Sign in with GitHub
     </button>
